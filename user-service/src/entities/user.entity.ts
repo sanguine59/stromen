6df3ab3a4b3fe098f34adc36b4542baeb4f052fb
Tsukz3n <owenroald@gmail.com>
2026-04-28 18:15:13 +0700

    feat: define user entity, register, and login

---
+++ user-service/src/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
